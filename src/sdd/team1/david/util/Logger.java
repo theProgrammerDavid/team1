b9fdf33621c7b4294e8b9d65043d8419461392e5
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-03 19:41:35 +0530

    fix: Logger.java

--- src/sdd/team1/david/util/Logger.java
+++ src/sdd/team1/david/util/Logger.java
@@ -1,20 +1,31 @@
 package sdd.team1.david.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
-import java.util.logging.Formatter;
-import java.util.logging.LogRecord;
-import java.util.logging.StreamHandler;
 
-public class Logger extends StreamHandler {
-    @Override
-    public  void publish(LogRecord record){
-        super.publish(record);;
+
+public class Logger {
+    private FileWriter fw;
+    private DateFormat dateFormat;
+
+    public Logger(String fileName) throws IOException {
+        fw = new FileWriter(fileName);
+        dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    }
+    private String getDateTime(){
+        return dateFormat.format(new Date());
+    }
+    public void Log(String topic, String message) throws IOException {
+        fw.write(getDateTime()+ "topic: "+topic+ " message: "+message);
     }
 
     @Override
-    public void flush(){
-        super.flush();
-    }
-    public void close() throws SecurityException{
-        super.close();
+    protected void finalize() throws Throwable {
+        fw.close();
     }
 }
