89a8c487eb34d6c7a2e241f2cff4d38af24ed4bf
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-31 01:36:55 +0530

    feat: working client server

--- src/sdd/team1/david/client/Client.java
+++ src/sdd/team1/david/client/Client.java
@@ -7,25 +7,15 @@ import java.util.List;
 import java.sql.*;
 public class Client {
 
-    public void sendStuff() throws IOException {
+    public void sendStuff() throws IOException, ClassNotFoundException {
         // need host and port, we want to connect to the ServerSocket at port 7777
         Socket socket = new Socket("localhost", 7777);
         System.out.println("Connected!");
+        InputStream inputStream = socket.getInputStream();
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
 
-        // get the output stream from the socket.
-        OutputStream outputStream = socket.getOutputStream();
-        // create an object output stream from the output stream so we can send an object through it
-        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-
-        // make a bunch of messages to send.
-        List<Message> messages = new ArrayList<>();
-        messages.add(new Message("Hello from the other side!"));
-        messages.add(new Message("How are you doing?"));
-        messages.add(new Message("What time is it?"));
-        messages.add(new Message("Hi hi hi hi."));
-
-        System.out.println("Sending messages to the ServerSocket");
-        objectOutputStream.writeObject(messages);
+        String x = (String)objectInputStream.readObject();
+        System.out.println(x);
 
         System.out.println("Closing socket and terminating program.");
         socket.close();
--- src/sdd/team1/david/server/Server.java
+++ src/sdd/team1/david/server/Server.java
@@ -1,8 +1,6 @@
 package sdd.team1.david.server;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ObjectInputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import sdd.team1.david.util.Message;
@@ -22,24 +20,14 @@ public class Server {
         ServerSocket ss = new ServerSocket(7777);
         System.out.println("ServerSocket awaiting connections...");
 
-        Socket socket = ss.accept(); // blocking call, this will wait until a connection is attempted on th$
-        System.out.println("Connection from " + socket + "!");
+        Socket socket = ss.accept(); // blocking call, this will wait until a connection is attempted on th$is
 
-        // get the input stream from the connected socket
-        InputStream inputStream = socket.getInputStream();
-        // create a DataInputStream so we can read data from it.
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        String x = "a piece of data";
+        OutputStream outputStream = socket.getOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(x);
 
-        // read the list of messages from the socket
-        List<Message> listOfMessages = (List<Message>) objectInputStream.readObject();
-        System.out.println("Received [" + listOfMessages.size() + "] messages from: " + socket);
-        // print out the text of every message
-        System.out.println("All messages:");
-        listOfMessages.forEach((msg)-> System.out.println(msg.getText()));
-
-        System.out.println("Closing sockets.");
         socket.close();
-
         ss.close();
     }
 }
