6e26e96bfe8e9e4dd251870a08c3ce55e0614805
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-31 00:13:06 +0530

    [WIP] Server

--- src/sdd/team1/david/server/Server.java
+++ src/sdd/team1/david/server/Server.java
@@ -1,5 +1,45 @@
 package sdd.team1.david.server;
 
-public class Server {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import sdd.team1.david.util.Message;
+import java.util.List;
 
+public class Server {
+    public void start(){
+        try{
+            while(true){
+                server();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void server() throws IOException, ClassNotFoundException {
+        ServerSocket ss = new ServerSocket(7777);
+        System.out.println("ServerSocket awaiting connections...");
+
+        Socket socket = ss.accept(); // blocking call, this will wait until a connection is attempted on th$
+        System.out.println("Connection from " + socket + "!");
+
+        // get the input stream from the connected socket
+        InputStream inputStream = socket.getInputStream();
+        // create a DataInputStream so we can read data from it.
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+
+        // read the list of messages from the socket
+        List<Message> listOfMessages = (List<Message>) objectInputStream.readObject();
+        System.out.println("Received [" + listOfMessages.size() + "] messages from: " + socket);
+        // print out the text of every message
+        System.out.println("All messages:");
+        listOfMessages.forEach((msg)-> System.out.println(msg.getText()));
+
+        System.out.println("Closing sockets.");
+        socket.close();
+
+        ss.close();
+    }
 }
