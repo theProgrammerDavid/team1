90af19973e06a009d24ac62063c1c1cc99259418
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-31 22:21:26 +0530

    Merge branch 'dev' of /home/david/Development/java-test with conflicts.

--- src/sdd/team1/david/client/Client.java
+++ src/sdd/team1/david/client/Client.java
@@ -1,20 +1,59 @@
 package sdd.team1.david.client;
+
 import sdd.team1.david.util.*;
+
 import java.net.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.*;
+
 public class Client {
 
-    public void sendStuff() throws IOException, ClassNotFoundException {
-        // need host and port, we want to connect to the ServerSocket at port 7777
-        Socket socket = new Socket("localhost", 7777);
-        System.out.println("Connected!");
-        InputStream inputStream = socket.getInputStream();
-        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+    private Socket socket;
 
-        String x = (String)objectInputStream.readObject();
+    InputStream inputStream;
+    OutputStream outputStream;
+
+    ObjectOutputStream objectOutputStream;
+    ObjectInputStream objectInputStream;
+
+    public void sendFile(String host, int port) throws IOException, ClassNotFoundException {
+        //need host and port, we want to connect to the ServerSocket at port 7777
+        socket = new Socket("localhost", 7777);
+        System.out.println("Connected!");
+
+        inputStream = socket.getInputStream();
+        outputStream = socket.getOutputStream();
+
+        objectOutputStream = new ObjectOutputStream(outputStream);
+        objectInputStream = new ObjectInputStream(inputStream);
+
+        //we want to send a file to the server so we tell it to receive a file
+        objectOutputStream.writeObject("receive");
+
+        String res = ""; //fill this with the db dump
+        objectOutputStream.writeObject(res);
+
+        System.out.println("Closing socket and terminating program.");
+        socket.close();
+    }
+
+    public void receiveFile(String host, int port) throws IOException, ClassNotFoundException {
+        //need host and port, we want to connect to the ServerSocket at port 7777
+        socket = new Socket(host, port);
+        System.out.println("Connected!");
+
+        inputStream = socket.getInputStream();
+        outputStream = socket.getOutputStream();
+
+        objectOutputStream = new ObjectOutputStream(outputStream);
+        objectInputStream = new ObjectInputStream(inputStream);
+
+        //we want to receive a file so we tell the server to send us an object
+        objectOutputStream.writeObject("send");
+
+        String x = (String) objectInputStream.readObject();
         System.out.println(x);
 
         System.out.println("Closing socket and terminating program.");
