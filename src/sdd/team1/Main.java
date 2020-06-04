3335502dd375bb48512cdd59c9bd0a2401a56df7
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-06-05 02:19:51 +0530

    samlpe: Tester code

--- src/sdd/team1/Main.java
+++ src/sdd/team1/Main.java
@@ -2,29 +2,42 @@ package sdd.team1;
 
 import sdd.team1.david.server.Server;
 import sdd.team1.david.client.Client;
+import sdd.team1.david.util.MyTask;
+import sdd.team1.david.util.MyTaskExecutor;
+import sdd.team1.tester.Tester;
 
 import java.io.File;
+
+class Test implements MyTask {
+    public void execute() {
+        System.out.println("Hello workd");
+    }
+}
 
 public class Main {
 
 
     public static void main(String[] args) {
         try {
-//            Runtime.getRuntime().exec("/usr/bin/mysqldump --databases SDD_PROJECT --user=sddproject --password=password > ~/Development/java-test/lol.sql");
-//            Runtime.getRuntime().exec("mysqldump -u sddproject -p password SDD_PROJECT > ~/Development/java-test/dump.sql");
+//            Runtime.getRuntime().exec("/usr/bin/mysqldump --databases SDD_PROJECT --user=sddproject --password=password > ~/Development/java-test/lol.sql")
+//            Runtime.getRuntime().exec("mysqldump --databases SDD_PROJECT --user=sddproject --password=password > ~/Development/java-test/dump.sql");
 
-//
-            Client c = new Client();
-            c.receiveFile("localhost", 7777);
+            Tester t= new Tester();
+            t.testClient();
 
+// receive file is working. Work on Client::sendFile
+//            Client c = new Client();
+//            c.createDump();
+//            c.receiveFile("localhost", 7777);
+//            c.sendFile("localhost", 7777);
 //            Server s = new Server();
 //            s.createDump();
 //            s.start();
 
-
-            //  MyTaskExecutor te = new MyTaskExecutor(t);
-//  te.startExecutionAt(16,45,30);
-//  te.stop();
+//            Test t = new Test();
+//            MyTaskExecutor te = new MyTaskExecutor(t);
+//            te.startExecutionAt(21, 52, 15);
+//            te.stop();
 //            Calendar rightNow = Calendar.getInstance();
 //            int hour = rightNow.get(Calendar.HOUR_OF_DAY);
 //            System.out.println(hour);
