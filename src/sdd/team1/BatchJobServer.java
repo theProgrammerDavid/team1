f297c6335b0b9b8db5fe279762a477eee62d6b61
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-06-05 02:20:42 +0530

    feat: Interface for server side batch jobs

--- src/sdd/team1/BatchJobServer.java
+++ src/sdd/team1/BatchJobServer.java
@@ -1,4 +1,16 @@
 package sdd.team1;
 
-public interface BatchJobServer {
+import sdd.team1.david.server.Server;
+import sdd.team1.david.util.MyTask;
+
+public interface BatchJobServer extends MyTask {
+    Server s = null;
+    String pwd=null;
+    public void start();
+    public String checkForOld();
+    public void createDump();
+    public void dumpTransferFailed();
+    public void mysqlInport();
+    public void server();
+
 }
